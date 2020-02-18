578e861c033776d198fdc7fcd57ccf78c0b361c5
Fulvio <fulviocanducci@hotmail.com>
2020-02-17 21:33:51 -0300

    Source

--- src/components/Source.vue
+++ src/components/Source.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>Source</div>
+  <div id="app">
+    <div>
+      <div>
+        <input
+          type="file"
+          class="hidden"
+          ref="uploadImgInput"
+          @change="updateCurrImg($event)"
+          accept="image/*"
+        />
+      </div>
+      <div>
+        <button @click="updateGeneral()">Pressione</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      avatarUrl: ""
+    };
+  },
+  methods: {
+    updateCurrImg(event) {
+      this.avatarUrl = event.target.files[0];
+    },
+    updateGeneral() {
+      let fd = new FormData();
+      fd.append("image", this.avatarUrl);
+      fd.append("name", "name");
+      fd.append("surname", "surname");
+      fd.append("phoneNumber", "1836962585");
+      fd.append("gender", "male");
+
+      var myInit = {
+        method:
+          "PUT" /*
+        headers: {
+          "Content-Type": "multipart/form-data"
+        },*/,
+        mode: "cors",
+        cache: "default",
+        body: fd
+      };
+      fetch("http://localhost:62941/api/source/update-general", myInit)
+        .then(c => c.json())
+        .then(c => alert(c));
+    }
+  }
+};
 </script>
 
 <style>
